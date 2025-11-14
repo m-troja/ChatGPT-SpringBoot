b8992bc97af8a1957902ea6c71d99b0b0a3c5d2b
Michal Trojanowski <mtroja98@gmail.com>
2025-11-14 22:59:35 +0100

    Github test

--- src/test/java/com/michal/openai/github/impl/GithubServiceImplTest.java
+++ src/test/java/com/michal/openai/github/impl/GithubServiceImplTest.java
@@ -1,9 +1,12 @@
 package com.michal.openai.github.impl;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.michal.openai.github.entity.GithubBranch;
 import com.michal.openai.github.entity.GithubCommit;
 import com.michal.openai.github.entity.GithubRepoDto;
 import com.michal.openai.github.entity.GithubRepoResponse;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -11,10 +14,17 @@ import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.web.client.RestClient;
 
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
 @Import(GithubServiceImplTest.TestConfig.class)
 @RestClientTest(GithubServiceImpl.class)
@@ -22,35 +32,52 @@ public class GithubServiceImplTest {
 
     @Autowired MockRestServiceServer server;
     @Autowired GithubServiceImpl service;
+    @Autowired ObjectMapper objectMapper;
+    private String baseUrl;
+
+    @BeforeEach
+    public void init() {
+        baseUrl = "https://api.github.com";
+    }
 
     @Test
-    public void shouldReturnTwoUserRepos() {
-        var githubResponse1 = new GithubRepoResponse(1L, "Repo1", "FullName1", "Desc1", "Html1", "CloneUrl1", "SshUrl1", "Language1", 1, 1, 1, 1, false, new GithubRepoResponse.Owner(1L, "login1", "htmlUrl1", "avatarUrl1"));
-        var githubResponse2 = new GithubRepoResponse(1L, "Repo1", "FullName1", "Desc1", "Html1", "CloneUrl1", "SshUrl1", "Language1", 1, 1, 1, 1, true, new GithubRepoResponse.Owner(1L, "login1", "htmlUrl1", "avatarUrl1"));
-        List<GithubRepoResponse> repoResponses = List.of(githubResponse1, githubResponse2);
+    public void shouldReturnTwoUserRepos() throws JsonProcessingException {
+
+        var owner = new GithubRepoResponse.Owner(1L, "login1", "htmlUrl1", "avatarUrl1");
+        String uri = "/users/" + owner.login() + "/repos";
+
+        var githubResponseNotForked = new GithubRepoResponse(1L, "Repo1", "FullName1", "Desc1", "Html1", "CloneUrl1", "SshUrl1", "Language1", 1, 1, 1, 1, false, owner);
+        var githubResponseForked = new GithubRepoResponse(1L, "Repo1", "FullName1", "Desc1", "Html1", "CloneUrl1", "SshUrl1", "Language1", 1, 1, 1, 1, true,  owner);
 
         var githubBranch1 = new GithubBranch("main1", new GithubCommit("commit1", "url1"));
         var githubBranch2 = new GithubBranch("main2", new GithubCommit("commit2", "url2"));
         var githubBranch3 = new GithubBranch("main3", new GithubCommit("commit3", "url3"));
-        List<GithubBranch> branchesRepo1 = List.of(githubBranch1, githubBranch2);
-        List<GithubBranch> branchesRepo2 = List.of(githubBranch3);
+        var branchesNotForked = List.of(githubBranch1, githubBranch2);
+        var branchesRForked = List.of(githubBranch3);
 
         List<GithubRepoDto> notForkedDtos = List.of(
-                new GithubRepoDto(githubResponse1.name(), githubResponse1.description(), List.of(githubBranch1, githubBranch2), githubResponse1.fork())
+                new GithubRepoDto(githubResponseNotForked.name(), githubResponseNotForked.description(), branchesNotForked, githubResponseNotForked.fork())
         );
 
         List<GithubRepoDto> forkedDtos = List.of(
-                new GithubRepoDto(githubResponse2.name(), githubResponse2.description(), List.of(githubBranch3), githubResponse2.fork())
+                new GithubRepoDto(githubResponseForked.name(), githubResponseForked.description(), branchesRForked, githubResponseForked.fork())
         );
 
+        server.expect(requestTo(baseUrl + uri))
+                .andExpect(method(HttpMethod.GET))
+                .andRespond(withSuccess(objectMapper.writeValueAsString(notForkedDtos), MediaType.APPLICATION_JSON));
 
+
+        List<GithubRepoDto> dtosFromService = service.getUserReposWithBranches(owner.login());
+        server.verify();
+        assertThat(dtosFromService).hasSize(1);
     }
 
     @TestConfiguration
     static class TestConfig {
         @Bean
-        @Qualifier("taskSystemRestClient")
-        RestClient taskSystemRestClient(RestClient.Builder builder) {
+        @Qualifier("githubRestClient")
+        RestClient githubRestClient(RestClient.Builder builder) {
             String baseUrl = "https://api.github.com";
             return builder.baseUrl(baseUrl).build();
         }
