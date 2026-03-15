8239bcfc810d453175762ec498accc745fabe81a
Michal Trojanowski <mtroja98@gmail.com>
2026-03-15 18:54:50 +0100

    GptTools refactor

--- src/main/java/com/michal/openai/async/AsyncConfig.java
+++ src/main/java/com/michal/openai/async/AsyncConfig.java
@@ -1,15 +1,19 @@
 package com.michal.openai.async;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
 
+@Slf4j
 @Configuration
 @EnableAsync
-public class AsyncConfig {
+public class AsyncConfig implements AsyncConfigurer {
 
     @Bean(name = "defaultExecutor")
     public Executor slackExecutor() {
@@ -20,5 +24,11 @@ public class AsyncConfig {
         executor.setThreadNamePrefix("SlackExecutor-");
         executor.initialize();
         return executor;
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return (ex, method, params) ->
+                log.error("Async error in method {} with params {}", method, params, ex);
     }
 }
